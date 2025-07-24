81c1c0da3ec537528f1e5ab591ea7ccb119b44ae
OblivionGrimes <mattstreet.387@gmail.com>
2025-07-24 19:41:32 -0300

    Tela de cadastro e login

--- Cadastro.php
+++ Cadastro.php
@@ -8,8 +8,10 @@
 <body style='background-color: hsla(50, 33%, 25%, 0.75);'> <!-- temporario esse style -->
     <div>
         <Form method='POST'>
+            <Label>Nome:</label>
+            <Input name='L_NOME' id='L_NOME' type='text'></Input><br>
             <Label>Email:</label>
-            <Input name='L_EMAIL' id='L_EMAIL' type='email' value='<?php echo $_POST['L_EMAIL'] ?? ''; ?>'></Input><br>
+            <Input name='L_EMAIL' id='L_EMAIL' type='email'></Input><br>
             <Label>Senha:</label>
             <Input name='L_SENHA' id='L_SENHA' type='password'></Input><br>
             <Input name='BT_SALVAR' type='submit' value='Acessar'></Input>
