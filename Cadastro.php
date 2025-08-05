28d7d5fcca43fe215f6bccb2869afba6854f625c
OblivionGrimes <mattstreet.387@gmail.com>
2025-08-05 12:31:53 -0300

    adições novas

--- Cadastro.php
+++ Cadastro.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="./CadLog/Cadastro.css">
 </head>
 
 <?php
     include "Conexao.php";
+
+    if(isset($_GET['error'])){
+        echo "<span style='color: red;'> Email já cadastrado anteriormente! </span>";
+    }
+
 
     if(isset($_POST['BT_SALVAR'])){
 
@@ -17,21 +23,31 @@
         $L_TIPO = $_POST['L_TIPO_USER'];
         $L_DATA = date("Y-m-d H:i:s");
 
-        $stmt = $mysqli->prepare("insert into projeto_tmp.sys_user (L_NOME, L_EMAIL, L_SENHA, L_TIPO, L_DATA) values (?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssis",$L_NOME, $L_EMAIL, $L_SENHA, $L_TIPO, $L_DATA);
-        $stmt->execute();
+        $array_valida = 
+            $mysqli->query("select 
+                                1 
+                            from 
+                                projeto_tmp.sys_user 
+                            where 
+                                L_EMAIL = '".$L_EMAIL."' and 
+                                L_TIPO = '".$L_TIPO."' 
+                            limit 1");
+        $dados = $array_valida->fetch_all();
 
-        header("Location: Cadastro.php?success=1");
+        if($dados[0][0] == 1){
+            header("Location: Cadastro.php?error=1");
+        }else{
+            $stmt = $mysqli->prepare("insert into projeto_tmp.sys_user (L_NOME, L_EMAIL, L_SENHA, L_TIPO, L_DATA) values (?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssis",$L_NOME, $L_EMAIL, $L_SENHA, $L_TIPO, $L_DATA);
+            $stmt->execute();
+
+            header("Location: Login.php?success=1");
+        }
     }
 ?>
 
 <body style='background-color: hsla(50, 33%, 25%, 0.75);'> <!-- temporario esse style -->
-    <div>
-        <?php
-            if(isset($_GET['success'])){
-                echo "Cadastro realizado com sucesso!!";
-            }
-        ?>
+    <div class="div_principal">
         <Form method='POST'>
             <Label>Nome:</label>
             <Input name='L_NOME' id='L_NOME' type='text'></Input><br>
@@ -46,6 +62,10 @@
             <label for="L_TIPO_USER">Não</label><br>
             <Input name='BT_SALVAR' type='submit' value='Salvar'></Input>
         </Form>
+
+        <div class="div_link">
+            <a href="Login.php">Retorna a tela de login</a>
+        </div>
     </div>
 </body>
 <?php
--- Login.php
+++ Login.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./CadLog/Cadastro.css">
 </head>
 <body style='background-color: hsla(50, 33%, 25%, 0.75);'> <!-- temporario esse style -->
-    <div>
+
+    <?php
+        include "Conexao.php";
+
+        if(isset($_GET['success'])){
+            echo "Cadastro realizado com sucesso!!";
+        }
+
+        if(isset($_REQUEST['BT_SALVAR'])){
+
+        }
+    ?>
+
+    <div class="div_principal">
         <Form method='POST'>
             <Label>Email:</label>
             <Input name='L_EMAIL' id='L_EMAIL' type='email' value='<?php echo $_POST['L_EMAIL'] ?? ''; ?>'></Input><br>
